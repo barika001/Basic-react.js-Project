339469d141d3e46af096f9a5fee99fd3bf1f4ed2
Barika Khalidi <001barika@gmail.com>
2021-06-05 22:48:22 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript & react.js</h1>
+      <p>This was an awesome bootcamp. Thanks to Shaurya Sinha.</p>
+    </div>
+  );
+}
+
+export default Note;
